a96907f7e2a854d2ae1d0b4c2565ee7086ab5e61
Samuel Giddins <segiddins@segiddins.me>
2016-07-19 17:01:51 -0500

    [RubyGems] Avoid warning for Installer.new with a path

--- lib/bundler/rubygems_gem_installer.rb
+++ lib/bundler/rubygems_gem_installer.rb
@@ -3,6 +3,14 @@ require "rubygems/installer"
 
 module Bundler
   class RubyGemsGemInstaller < Gem::Installer
+    unless respond_to?(:at)
+      def self.at(path, options = {})
+        security_policy = options[:security_policy]
+        package = Gem::Package.new(path, security_policy)
+        new(package, options)
+      end
+    end
+
     def check_executable_overwrite(filename)
       # Bundler needs to install gems regardless of binstub overwriting
     end
